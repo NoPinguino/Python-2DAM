d5d4f8e21f871d7a398786a910e1bff353e82077
NoPinguino <misael.delamorena@gmail.com>
2025-09-23 18:38:02 +0200

    Tarea-031

---
+++ python/otros/pruebas/prueba-listas/prueba-listas.py
@@ -0,0 +1,10 @@
+mi_lista = [1,0,2,6,3,2]
+nueva_lista1 = mi_lista[1:3] #Coge de la posición 1 -> 3 (excluyendo la última)
+nueva_lista2 = mi_lista[1:6:2] #Coge de la posición 1 -> 6 (incrementos de 2)
+
+print(mi_lista)
+print(nueva_lista1)
+print(nueva_lista2)
+print(mi_lista[:5])
+print(mi_lista[:6:2])
+print(mi_lista[::-1])
