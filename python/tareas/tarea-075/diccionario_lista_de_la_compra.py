e9226b176dc6008bb869f6da50f2d927657c86ee
NoPinguino <misael.delamorena@gmail.com>
2025-11-19 10:20:10 +0100

    ejercicios 18 noviembre

---
+++ python/tareas/tarea-075/diccionario_lista_de_la_compra.py
@@ -0,0 +1,8 @@
+diccionario = {
+    "huevo" : 6,
+    "pan" : 2,
+    "leche" : 1,
+    "café" : 1,
+}
+for key in diccionario:
+    print(f"Has comprado {diccionario[key]} {key}s")
---
+++ python/tareas/tarea-077/suma_edades.py
@@ -0,0 +1,7 @@
+diccionario = {
+    "hugo" : 21,
+    "misael" : 19,
+    "raúl": 30,
+}
+suma_edades = sum(diccionario.values())
+print(suma_edades)
